e9d9b85daa60acd800f8f780edf48ae122c5d2a6
djordjereact <djole07mihailovic@gmail.com>
2019-12-10 22:03:10 +0100

    Change value dynamic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I`m React App</h1>
-        <p>This is really working!!!</p>
-        <Person/>
-        <Person/>
-        <Person/>
-    </div>
-  );
+class App extends Component {
+    state ={
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephanie', age: 26 }
+        ],
+        otherState: 'some other value'
+
+    }
+
+    switchNameHandler = () => {
+        //console.log('Was clicked');
+        this.setState({persons: [
+                { name: 'Maximilan', age: 28 },
+                { name: 'Manu', age: 29 },
+                { name: 'Stephanie', age: 27 }
+            ]
+        }
+       )
+      }
+
+  render () {
+      return (
+          <div className="App">
+              <h1>Hi, I`m React App</h1>
+              <p>This is really working!!!</p>
+              <button onClick={this.switchNameHandler}>Switch Name</button>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div>
+        );
+  }
+
+
+
   // return React.createElement('div',{className: "App"}, React.createElement('h1',null, 'Does this work now?')) 
 }
 
