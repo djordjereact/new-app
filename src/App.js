7de5de00132c92aa70e78b9bdda97b742d207c50
djordjereact <djole07mihailovic@gmail.com>
2019-12-26 22:29:04 +0100

    Now you can click the paragraph

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
 
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         //console.log('Was clicked');
         this.setState({persons: [
-                { name: 'Maximilan', age: 28 },
+                { name: newName, age: 28 },
                 { name: 'Manu', age: 29 },
                 { name: 'Stephanie', age: 27 }
             ]
@@ -29,10 +29,15 @@ class App extends Component {
           <div className="App">
               <h1>Hi, I`m React App</h1>
               <p>This is really working!!!</p>
-              <button onClick={this.switchNameHandler}>Switch Name</button>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+              <button onClick={() => this.switchNameHandler()}>Switch Name</button>
+              <Person name={this.state.persons[0].name}
+                      age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name}
+                      age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Max!')}>
+                  My hobbies: Racing</Person>
+              <Person name={this.state.persons[2].name}
+                      age={this.state.persons[2].age}/>
           </div>
         );
   }
